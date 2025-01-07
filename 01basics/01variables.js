ebe6fd01f7407e297af9198366f33ef0f915fdff
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-01-07 14:28:56 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,21 @@
+const accountId =1122345
+let accountEmail="harikrishnakm34@gmail.com"
+var accountPassword ="12345"
+accountCity="mumbai"
+//accountId=2
+let accountState
+console.log(accountId);
+
+accountEmail ="hcbc@gmail.com"
+accountPassword="123454"
+accountCity="kannur"
+
+console.table([accountEmail,accountPassword,accountCity])
+
+
+/*
+prefer  not to use var 
+because of issue of block space
+*/
+
+
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we using nodejs,not browser
+
+console.log(3+ 3)  // code readability should be high
+
+    console.log("Harikrishna")
+
+
+
+
+    let name="harikrishna"   //string ho gaya
+    let age= 20               //number hogaya
+    let isLoggedIn=false
+
+// number=> 2 to the power 53 (isse bada number lagega tho)
+//bigint (use karege)
+// string=> preffered in ("")
+// boolean=> true/false
+// null => standalone value (undefined nhi hai but khaali hai )
+// undefined (abhi value assign nhi hui hai )
+// symbol
+
+
+//object
+
+   console.log(typeof age);  //typeof type bata tha hai string ya num ya object
+
+   console.log(typeof undefined);  // undefined
+   console.log(typeof null);  // object
+   
+   
+
+    
+
+
